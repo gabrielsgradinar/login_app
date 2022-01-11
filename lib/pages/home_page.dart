b0f09e0eebc5c9c1d0908a25bc4fb114a9150b09
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2022-01-11 17:12:18 -0300

    using repository pattern with mock data and use asserts

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:login/controllers/home_controller.dart';
+import 'package:login/models/post_model.dart';
+import 'package:login/repositories/home_repository_mock.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final HomeController _homeController = HomeController(HomeRepositoryMock());
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _homeController.fetch();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ValueListenableBuilder<List<PostModel>>(
+        valueListenable: _homeController.posts,
+        builder: (_, list, __) {
+          return ListView.builder(
+            itemCount: list.length,
+            itemBuilder: (_, idx) => ListTile(
+              title: Text(list[idx].title),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
---
+++ lib/repositories/home_repository_mock.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:login/models/post_model.dart';
+import 'package:login/repositories/home_repository.dart';
+
+class HomeRepositoryMock implements HomeRepository {
+  @override
+  Future<List<PostModel>> getList() async {
+    var value = await rootBundle.loadString('assets/data.json');
+    List postJson = jsonDecode(value);
+    return postJson.map((e) => PostModel.fromJson(e)).toList();
+  }
+}
